26c25eddf9636cc163595dede26d81f582a5e709
Quang Phan <qp2194@gmail.com>
2023-06-23 00:43:20 -0500

    login user

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,33 +1,32 @@
-const User = require('../models/User')
-const { StatusCodes } = require('http-status-codes')
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body })
-  const token = user.createJWT()
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
-}
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
 
 const login = async (req, res) => {
-//   const { email, password } = req.body
+  const { email, password } = req.body;
 
-//   if (!email || !password) {
-//     throw new BadRequestError('Please provide email and password')
-//   }
-//   const user = await User.findOne({ email })
-//   if (!user) {
-//     throw new UnauthenticatedError('Invalid Credentials')
-//   }
-//   const isPasswordCorrect = await user.comparePassword(password)
-//   if (!isPasswordCorrect) {
-//     throw new UnauthenticatedError('Invalid Credentials')
-//   }
-//   // compare password
-//   const token = user.createJWT()
-//   res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
-    res.send("login");
-}
+  if (!email || !password) {
+    throw new Error("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Invalid Credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new Error("Invalid Credentials");
+  }
+  
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
 
 module.exports = {
   register,
   login,
-}
+};
