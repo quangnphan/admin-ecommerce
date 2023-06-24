6b122a6c123b3b72adc8684d15b2c33015b595bd
Quang Phan <qp2194@gmail.com>
2023-06-23 20:17:42 -0500

    fixed productSchema

---
+++ backend/models/Customer.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zipcode: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Customer", customerSchema);
