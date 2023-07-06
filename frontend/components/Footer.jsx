5b1986e004f162cad8d785b7ad966cd39c0a7f2a
Quang Phan <qp2194@gmail.com>
2023-07-06 18:45:06 -0500

    fix Next

--- frontend/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => (
-    <div className="mt-24">
-      <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-        © 2023 All rights reserved by E-commerce
-      </p>
-    </div>
-  );
-
-export default Footer;
