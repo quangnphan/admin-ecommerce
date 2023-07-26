5fea5e882e37e727b2fcd74ce26580ac44568ad9
Quang Phan <qp2194@gmail.com>
2023-07-25 22:14:39 -0500

    populate products detail

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -8,22 +8,33 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    throw new Error("Please provide email and password");
-  }
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw new Error("Invalid Credentials");
-  }
-  const isPasswordCorrect = await user.comparePassword(password);
-  if (!isPasswordCorrect) {
-    throw new Error("Invalid Credentials");
-  }
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: "Please provide email and password" });
+    }
+    
+    const user = await User.findOne({ email }).select("+password"); // Include the password field in the query
+    if (!user) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid Credentials" });
+    }
+    
+    const isPasswordCorrect = await user.comparePassword(password);
   
-  const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+    if (!isPasswordCorrect) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid Credentials" });
+    }
+    
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+  } catch (error) {
+    console.error(error);
+    // Check if the error is related to database operations
+    if (error.name === "MongoError" || error.name === "CastError") {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Database Error" });
+    }
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Internal Error" });
+  }
 };
 
 module.exports = {
--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -8,35 +8,48 @@ const getCategories = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  let categoryFilter = {};
-  // Check if a category is specified in the URL
-  if (req.params.category) {
-    const category = await Category.findOne({ name: { $regex: new RegExp('^' + req.params.category, 'i') } });
-    // If the category does not exist, return an error
-    if (!category) {
-      return res.status(StatusCodes.NOT_FOUND).json({ error: 'Category not found' });
+  try {
+    let categoryFilter = {};
+    // Check if a category is specified in the URL
+    if (req.params.category) {
+      const category = await Category.findOne({ name: { $regex: new RegExp('^' + req.params.category, 'i') } });
+      // If the category does not exist, return an error
+      if (!category) {
+        return res.status(StatusCodes.NOT_FOUND).json({ error: 'Category not found' });
+      }
+      categoryFilter = { category: category._id };
     }
-    
-    categoryFilter = { category: category._id };
+
+    // Populate data from another collection and apply category filter
+    const products = await Product.find(categoryFilter).populate({
+      path: 'category',
+      options: { lean: false },
+    }).populate({
+      path: 'created_by',
+      select: 'name', // Include only the 'name' field from the User model
+      options: { lean: false },
+    });
+
+    res.status(StatusCodes.OK).json({ products, total: products.length });
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Failed to fetch products" });
   }
-
-  // Populate data from another collection and apply category filter
-  const products = await Product.find(categoryFilter).populate({
-    path: 'category',
-    options: { lean: false },
-  });
-
-  res.status(StatusCodes.OK).json({ products, total: products.length });
 };
 
 const getProduct = async (req, res) => {
-  const product = await Product.findOne({
-    _id: req.params.id,
-  });
-  if (!product) {
-    throw new Error(`No product found!`);
+  try {
+    const product = await Product.findOne({
+      _id: req.params.id,
+    }).populate('created_by', 'name').populate('category', 'name');
+    if (!product) {
+      return res.status(StatusCodes.NOT_FOUND).json({ error: "Product not found" });
+    }
+    res.status(StatusCodes.OK).json({ product });
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Failed to fetch product" });
   }
-  res.status(StatusCodes.OK).json({ product });
 };
 
 const createProduct = async (req, res) => {
@@ -46,17 +59,42 @@ const createProduct = async (req, res) => {
 };
 
 const updateProduct = async (req, res) => {
-  res.send("update product");
+  const productId = req.params.id; // Assuming the product ID is passed as a URL parameter
+  const updatedFields = req.body; // Assuming the updated fields are sent in the request body
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      updatedFields,
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      // If the product with the specified ID does not exist, return an error
+      return res.status(StatusCodes.NOT_FOUND).json({ error: "Product not found" });
+    }
+
+    res.status(StatusCodes.OK).json(updatedProduct); // Return the updated product as the response
+  } catch (error) {
+    // Handle any errors that occur during the update process
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Failed to update product" });
+  }
 };
 
 const deleteProduct = async (req, res) => {
-  const product = await Product.findByIdAndRemove({
-    _id: req.params.id,
-  });
-  if (!product) {
-    throw new Error(`No product found!`);
+  try {
+    const product = await Product.findByIdAndRemove({
+      _id: req.params.id,
+    });
+    if (!product) {
+      return res.status(StatusCodes.NOT_FOUND).json({ error: "Product not found" });
+    }
+    res.status(StatusCodes.OK).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Failed to delete product" });
   }
-  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
